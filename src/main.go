7c3f9fefe8225e7de2a3609e64ec781eb8aa1b6d
Yiling Zhou <pastgift@gmail.com>
2014-11-27 16:55:19 +0800

    add println for debug

--- src/main.go
+++ src/main.go
@@ -21,13 +21,15 @@ func main() {
     access_key_id     := access_key[0]
     access_key_secret := access_key[1]
 
-    fmt.Println("Useing AccessKeyId:",     access_key_id)
-    fmt.Println("Useing AccessKeySecret:", access_key_secret)
+    fmt.Println("Useing AccessKeyId:\n\t",     access_key_id)
+    fmt.Println("Useing AccessKeySecret:\n\t", access_key_secret)
+    fmt.Println("")
 
     ecs_client := ecs.NewEcsClient(access_key_id,access_key_secret)
     res, err := ecs_client.DescribeRegions()
     if err == nil {
-        fmt.Println(res)
+        fmt.Println("Request URL:\n\t", ecs_client.ApiUrl.String())
+        fmt.Println("API result:\n\t", res)
     }
 
     fmt.Println("----- Test End -----")
